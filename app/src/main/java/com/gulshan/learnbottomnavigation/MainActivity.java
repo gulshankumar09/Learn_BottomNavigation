67c43769ec575b96a386bf7c0ffdf854a498287f
Gulshan kmar <gk326747@gmail.com>
2021-06-18 12:58:50 +0530

    appbar configured

--- app/src/main/java/com/gulshan/learnbottomnavigation/MainActivity.java
+++ app/src/main/java/com/gulshan/learnbottomnavigation/MainActivity.java
@@ -3,6 +3,7 @@ package com.gulshan.learnbottomnavigation;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -19,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
         NavController navController = Navigation.findNavController(this,R.id.navHostFragment);
 
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.my_dest,
+                R.id.friends_dest).build();
+
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        NavigationUI.setupActionBarWithNavController(this,navController, appBarConfiguration);
+
+
+
     }
 }
